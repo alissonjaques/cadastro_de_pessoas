35a8ac9d9c7a045cab8cbcfe10c7dc0e9d09c72a
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-01-08 19:19:14 -0300

    Adicionando tela de cadastro de pessoas no projeto

---
+++ CRUD_FireBird/view/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace CadastroDePessoas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCientes = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnAltera = new System.Windows.Forms.Button();
+            this.btnExclui = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCientes)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvCientes
+            // 
+            this.dgvCientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCientes.Location = new System.Drawing.Point(13, 170);
+            this.dgvCientes.Name = "dgvCientes";
+            this.dgvCientes.RowHeadersWidth = 51;
+            this.dgvCientes.RowTemplate.Height = 24;
+            this.dgvCientes.Size = new System.Drawing.Size(685, 247);
+            this.dgvCientes.TabIndex = 0;
+            this.dgvCientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCientes_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnProcurar);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtTelefone);
+            this.groupBox1.Controls.Add(this.txtEndereco);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Location = new System.Drawing.Point(13, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(685, 142);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Clientes";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(182, 20);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(109, 24);
+            this.btnProcurar.TabIndex = 10;
+            this.btnProcurar.Text = "Pesquisar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(311, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Endereço";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(358, 105);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(312, 22);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(95, 105);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(210, 22);
+            this.txtTelefone.TabIndex = 3;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(95, 79);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(575, 22);
+            this.txtEndereco.TabIndex = 2;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(95, 49);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(575, 22);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(95, 20);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(69, 22);
+            this.txtID.TabIndex = 0;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(13, 422);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 35);
+            this.btnNovo.TabIndex = 2;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnAltera
+            // 
+            this.btnAltera.Location = new System.Drawing.Point(94, 422);
+            this.btnAltera.Name = "btnAltera";
+            this.btnAltera.Size = new System.Drawing.Size(75, 35);
+            this.btnAltera.TabIndex = 2;
+            this.btnAltera.Text = "Alterar";
+            this.btnAltera.UseVisualStyleBackColor = true;
+            this.btnAltera.Click += new System.EventHandler(this.btnAltera_Click);
+            // 
+            // btnExclui
+            // 
+            this.btnExclui.Location = new System.Drawing.Point(175, 422);
+            this.btnExclui.Name = "btnExclui";
+            this.btnExclui.Size = new System.Drawing.Size(75, 35);
+            this.btnExclui.TabIndex = 2;
+            this.btnExclui.Text = "Excluir";
+            this.btnExclui.UseVisualStyleBackColor = true;
+            this.btnExclui.Click += new System.EventHandler(this.btnExclui_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(623, 422);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 35);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(529, 422);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 34);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(710, 458);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExclui);
+            this.Controls.Add(this.btnAltera);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvCientes);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCientes)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCientes;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnAltera;
+        private System.Windows.Forms.Button btnExclui;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnProcurar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ CRUD_FireBird/view/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CadastroDePessoas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            preencheGrid();
+        }
+        //
+        private void preencheGrid()
+        {
+            try
+            {
+                dgvCientes.DataSource = AcessoFB.fb_GetDados().DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+        }
+        //
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            Cliente cliente = new Cliente();
+            cliente.ID = Convert.ToInt32(txtID.Text);
+            cliente.Nome = txtNome.Text;
+            cliente.Endereco = txtEndereco.Text;
+            cliente.Telefone = txtTelefone.Text;
+            cliente.Email = txtEmail.Text;
+
+            try
+            {
+                AcessoFB.fb_InserirDados(cliente);
+                preencheGrid();
+                MessageBox.Show("Cliente inserido com sucesso !", "Inserir", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+        }
+        //
+        private void dgvCientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Cliente cliente = new Cliente();
+            try
+            {
+                int codigo = Convert.ToInt32(dgvCientes.Rows[e.RowIndex].Cells[0].Value);
+                cliente = AcessoFB.fb_ProcuraDados(codigo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+            preencheDados(cliente);
+        }
+        //
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            Cliente cliente = new Cliente();
+            try
+            {
+                int codigo = (int)Int64.Parse(txtID.Text);
+                cliente = AcessoFB.fb_ProcuraDados(codigo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+            preencheDados(cliente);
+        }
+        //
+        private void preencheDados(Cliente cli)
+        {
+            txtID.Text = cli.ID.ToString();
+            txtNome.Text = cli.Nome;
+            txtEndereco.Text = cli.Endereco;
+            txtTelefone.Text = cli.Telefone;
+            txtEmail.Text = cli.Email;
+        }
+        //
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            btnNovo.Enabled = true;
+            foreach (Control myControl in groupBox1.Controls)
+            {
+                if (myControl as TextBox == null)
+                { }
+                else
+                {
+                    ((TextBox)myControl).Text = "";
+                }
+            }
+            txtID.Focus();
+        }
+        //
+        private void btnAltera_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cliente cliente = new Cliente();
+                cliente.ID = Convert.ToInt32(txtID.Text);
+                cliente.Nome = txtNome.Text;
+                cliente.Endereco = txtEndereco.Text;
+                cliente.Telefone = txtTelefone.Text;
+                cliente.Email = txtEmail.Text;
+                AcessoFB.fb_AlterarDados(cliente);
+                preencheGrid();
+                MessageBox.Show("Cliente alterado com sucesso !", "Alterar", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+        }
+        //
+        private void btnExclui_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int codigo = Convert.ToInt32(txtID.Text);
+                AcessoFB.fb_ExcluirDados(codigo);
+                preencheGrid();
+                MessageBox.Show("Cliente excluído com sucesso !", "Alterar", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
